81ebf4714894bf4e03ea92d9f8155df5034b669c
matveev_aa <matveev_aa@diakont.com>
2024-02-13 13:06:05 +0300

    feat: Добавить SwaggerOptions для хранения заголовка и номера версии. Будет использовать в паттерне Option

---
+++ src/Micro.Framework.Rest/Options/SwaggerOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Micro.Framework.Rest.Options
+{
+    public sealed record SwaggerOptions(string Title, string Version)
+    {
+        public const string Swagger = nameof(Swagger);
+
+        public SwaggerOptions()
+            : this(string.Empty, "1.0")
+        { }
+    }
+}
