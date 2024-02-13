2918f058cd137a9671457af450617ce26b9c48a8
matveev_aa <matveev_aa@diakont.com>
2024-02-13 12:32:34 +0300

    feat: Добавить TypeExtensions для формирования названия топика по типу объекта. Добавить ExpressionExtensions для формирования фильтра And

---
+++ src/Micro.Framework.Common/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Micro.Framework.Common.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression<Func<T, bool>> AndExpression<T>(this Expression<Func<T, bool>> query,
+          Expression<Func<T, bool>> exp)
+        {
+            return query != null ? query.And(exp) : exp;
+        }
+    }
+}
---
+++ src/Micro.Framework.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Micro.Framework.Common.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static string ConvertToTopicName(this Type type)
+        {            
+            var topicArray = type.FullName.Split('.');
+            var lowerTopicArray = topicArray
+                .Select(s => string.Concat(s[0].ToString().ToLower(), s.AsSpan(1)))
+                .ToArray();
+
+            return Regex.Replace(string.Join('.', lowerTopicArray), "\\.(?=[^.]*$)", ":");
+        }
+    }
+}
