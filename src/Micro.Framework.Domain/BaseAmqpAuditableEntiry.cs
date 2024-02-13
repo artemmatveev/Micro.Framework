4baff53d8804fd97da8b2273debccf6d650ff880
matveev_aa <matveev_aa@diakont.com>
2024-02-13 12:07:40 +0300

    feat: Добавить базовые классы для реализации Transaction Outbox

---
+++ src/Micro.Framework.Domain/BaseAmqpAuditableEntiry.cs
@@ -0,0 +1,63 @@
+﻿namespace Micro.Framework.Domain
+{
+    public abstract class BaseAmqpAuditableEntiry<TPKey> : BaseEntity<TPKey>, IBaseAmqpAuditableEntity
+    {
+        private Regex SERVER_NAME_REGEX = new Regex(@"^\w*\.\w*.\w*", RegexOptions.Compiled);
+        private string? _typeName;
+        private string? _assemblyName;
+
+        public BaseAmqpAuditableEntiry(string eventType, object payload, string topic)
+        {
+            EventType = eventType;
+            Payload = payload;
+            Topic = topic;
+        }
+
+        public string EventType { get; set; }
+        public object Payload { get; set; }
+        public string Topic { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+        public DateTime? SentTime { get; set; }
+
+
+        public string TypeName
+        {
+            get
+            {
+                if (_typeName is null)
+                {
+                    var exchangeArray = Topic.Replace(':', '.').Split('.');
+                    var upperExchangeArray = exchangeArray
+                        .Select(s => string.Concat(s[0].ToString().ToUpper(), s.AsSpan(1)))
+                        .ToArray();
+
+                    _typeName = string.Join('.', upperExchangeArray);
+                }
+
+                return _typeName;
+            }
+        }
+
+        public string AssemblyName
+        {
+            get
+            {
+                if (_assemblyName is null)
+                {
+                    _assemblyName = SERVER_NAME_REGEX.Match(TypeName).Value;
+                }
+
+                return _assemblyName;
+            }
+        }
+
+
+        public void Send()
+        {
+            if (SentTime is null)
+            {
+                SentTime = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ src/Micro.Framework.Domain/IBaseAmqpAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Micro.Framework.Domain
+{
+    public interface IBaseAmqpAuditableEntity
+    {
+        string EventType { get; set; }
+        object Payload { get; set; }
+        string Topic { get; set; }
+        DateTime CreationTime { get; set; }
+        DateTime? SentTime { get; set; }
+    }
+}
