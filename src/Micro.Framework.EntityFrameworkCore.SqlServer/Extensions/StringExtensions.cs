a7bced484fbf1819e10c3c9d09f88006064d37d2
matveev_aa <matveev_aa@diakont.com>
2024-02-13 13:59:01 +0300

    feat: Добавить ToSnakeCase, которые можно использовать при настройки condition EF (маппинг полей сущности на поля таблиц БД)

---
+++ src/Micro.Framework.EntityFrameworkCore.SqlServer/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Micro.Framework.EntityFrameworkCore.SqlServer.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        ///     Преобразует строку ToSnakeCase к виду to_shake_case.
+        ///     Для foreignKey добавляет постфикс _id.
+        /// </summary>        
+        public static string ToSnakeCase(this string str, bool asForeignKey = false)
+        {
+            Regex pattern = new Regex(@"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+");
+            string result = string.Join("_", pattern.Matches(str)).ToLower();
+
+            return asForeignKey ? result + "_id" : result;
+        }
+    }
+}
