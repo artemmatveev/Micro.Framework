af87980b9c1c48023d840050fcb462c87ec7f8f5
matveev_aa <matveev_aa@diakont.com>
2024-02-13 13:50:51 +0300

    refactor: Настроить ссылки

--- src/Micro.Framework.Grpc/GlobalUsings.cs
+++ src/Micro.Framework.Grpc/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using Grpc.Core;
 global using Grpc.Core.Interceptors;
-global using Iems.Framework.Core.Exceptions;
-global using Iems.Framework.Grpc.Core.Exceptions;
+global using Micro.Framework.Core.Exceptions;
+global using Micro.Framework.Grpc.Core.Exceptions;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
