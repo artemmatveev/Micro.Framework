431d7de1dc04f9b61b71e9f6a4953010839ef3c0
matveev_aa <matveev_aa@diakont.com>
2024-02-13 13:57:21 +0300

    feat: Добавить OrderByEx для выполнения сортировки по переданному полю

---
+++ src/Micro.Framework.EntityFrameworkCore.SqlServer/Extensions/QueryableExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Micro.Framework.EntityFrameworkCore.SqlServer.Extensions
+{    
+    public static class QueryableExtensions
+    {
+        public static IQueryable<TEntity> OrderByEx<TEntity>(this IQueryable<TEntity> source,
+            string orderByProperty, bool desc)
+        {
+            string command = desc ? "OrderByDescending" : "OrderBy";
+            var type = typeof(TEntity);
+
+            var property = type.GetProperty(orderByProperty);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+
+            var resultExpression = Expression.Call(typeof(Queryable), command,
+                            new Type[] { type, property.PropertyType },
+                            source.Expression, Expression.Quote(orderByExpression));
+
+            return source.Provider.CreateQuery<TEntity>(resultExpression);
+        }
+    }
+}
---
+++ src/Micro.Framework.EntityFrameworkCore.SqlServer/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Linq.Expressions;
+global using System.Text.RegularExpressions;
+global using Micro.Framework.Core.Exceptions;
+global using Micro.Framework.Domain;
+global using Microsoft.EntityFrameworkCore;
+global using Micro.Framework.Common.Model;
