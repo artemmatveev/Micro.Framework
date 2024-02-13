c1fffe387de61dcb4d86671e49a58c92eda99eaa
matveev_aa <matveev_aa@diakont.com>
2024-02-13 13:03:39 +0300

    feat: Добавить BaseController, который в конструктор принимает IMediator

---
+++ src/Micro.Framework.Rest/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿namespace Micro.Framework.Rest.Controllers
+{
+    public abstract class BaseController<TController> : ControllerBase where TController : ControllerBase
+    {
+        protected IMediator _mediator;
+
+        public BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
---
+++ src/Micro.Framework.Rest/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.Reflection;
+global using MediatR;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.OpenApi.Models;
